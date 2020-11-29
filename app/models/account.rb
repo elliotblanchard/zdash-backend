71096d595e4d0b0ab802e8a9b3e4c7a3ca091b58
Elliot Blanchard <git@elliotblanchard.com>
2020-11-29 14:34:45 -0500

    Implemented full account API call with account + transaction objects

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,10 +1,11 @@
 require 'json'
-require 'open-uri'
 require 'httparty'
+require 'pry'
+require_relative './account_transaction'
 
 class Account
 
-  attr_accessor :address, :balance, :firstSeen, :lastSeen, :sentCount, :recvCount, :minedCount, :totalSent, :totalRecv
+  attr_accessor :address, :balance, :firstSeen, :lastSeen, :sentCount, :recvCount, :minedCount, :totalSent, :totalRecv, :sentTrans, :recvTrans
 
   @@all = []
 
@@ -18,21 +19,45 @@ class Account
   end
 
   def self.find_by_address(address)
+    account_hash = Hash.new
+
     # make api call for account info
     response = HTTParty.get("https://api.zcha.in/v2/mainnet/accounts/#{address}")
     begin
       parsed = JSON.parse(response.body)
-      # return parsed["data"]["text"]
-      return self.new(parsed)
+      account_hash = parsed
     rescue JSON::ParserError => e
       return false
     end
 
-    # make api call for last 1,000 transactions sent
+    # make api call for last 20 transactions sent
+    # https://api.zcha.in/v2/mainnet/accounts/t3Vz22vK5z2LcKEdg16Yv4FFneEL1zg9ojd/sent?limit=20&offset=0&sort=timestamp&direction=descending
+    response = HTTParty.get("https://api.zcha.in/v2/mainnet/accounts/#{address}/sent?limit=20&offset=0&sort=timestamp&direction=descending")
+    begin
+      parsed = JSON.parse(response.body)
+      sentTrans = []
+      parsed.each do |transaction|
+        sentTrans.push(AccountTransaction.new(transaction))
+      end
+      account_hash[:sentTrans] = sentTrans
+    rescue JSON::ParserError => e
+      return false
+    end
 
-    # make api call for last 1,000 transations rec'd
+    # make api call for last 20 transations rec'd
+    # https://api.zcha.in/v2/mainnet/accounts/t3Vz22vK5z2LcKEdg16Yv4FFneEL1zg9ojd/recv?limit=20&offset=0&sort=timestamp&direction=descending
+    response = HTTParty.get("https://api.zcha.in/v2/mainnet/accounts/#{address}/recv?limit=20&offset=0&sort=timestamp&direction=descending")
+    begin
+      parsed = JSON.parse(response.body)
+      recvTrans = []
+      parsed.each do |transaction|
+        recvTrans.push(AccountTransaction.new(transaction))
+      end
+      account_hash[:recvTrans] = recvTrans
+      return self.new(account_hash)
+    rescue JSON::ParserError => e
+      return false
+    end
 
-    # return account object with account info + last 1,000 transactions rec'd and sent
-    # OR error if account does not exist
   end
 end
---
+++ app/models/account_transaction.rb
@@ -0,0 +1,15 @@
+class AccountTransaction
+
+  attr_accessor :hash, :mainChain, :fee, :type, :shielded, :index, :blockHash, :blockHeight, :version, :lockTime, :timestamp, :time, :vin, :vout, :vjoinsplit, :vShieldedOutput, :vShieldedSpend, :valueBalance, :value, :outputValue, :shieldedValue, :overwintered
+
+  @@all = []
+  
+  def initialize(account_hash)
+    account_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
