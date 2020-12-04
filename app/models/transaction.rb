77ab938ea0e481b28ef17565576c068ab900aaf4
Elliot Blanchard <git@elliotblanchard.com>
2020-12-03 22:14:16 -0500

    Classification code is working

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,12 +1,17 @@
 class Transaction < ApplicationRecord
+
   validates :zhash, presence: true
   validates :zhash, uniqueness: true 
 
   def self.classify_all
-    
+    transactions = all
+    transactions.each do |transaction| 
+      category = classify(transaction)
+      transaction.update(category: category)
+    end
   end
 
-  def self.classify(zhash)
+  def self.classify(transaction)
     # Some example transaction hashes:
     # d456a889ddc87ad41e379de5bb245781333fd883b67bf34eebabd1a6fb7e144a
     # d2ebc0cfd864027eb0887e1dcb772b4d1ca7bc016504889a6843583c2ca73bb4
@@ -16,23 +21,24 @@ class Transaction < ApplicationRecord
     # This code block gets moved into external module that backend
     # and API scraper can both share
 
-    transaction = self.find_by(zhash: zhash)
+    # transaction = self.find_by(zhash: zhash)
+
     if transaction
-      if transaction.vin.length.positive?
+      if transaction.vin.length > 2
         parsed = transaction.vin.split(',')
         if parsed[0].length > 18
           # vin arr contains coinbase field w/address
           # !!! check this carefully to see that it works
-          if transaction.vout.length.positive?
+          if transaction.vout.length > 2
             'transparent_coinbase'
           else
             'shielded_coinbase'
           end
         else
-          if transaction.vout.length.positive?
+          if transaction.vout.length > 2
             'transparent'
           else
-            if transaction.vjoinsplit.length.positive?
+            if transaction.vjoinsplit.length > 2
               'sprout_shielding'
             else
               'sapling_shielding'
@@ -40,14 +46,14 @@ class Transaction < ApplicationRecord
           end
         end
       else
-        if transaction.vout.length.positive?
-          if transaction.vjoinsplit.positive?
+        if transaction.vout.length > 2
+          if transaction.vjoinsplit.length > 2
             'sprout_deshielding'
           else
             'sapling_deshielding'
           end
         else
-          if transaction.vjoinsplit.positive?
+          if transaction.vjoinsplit.length > 2
             if transaction.vShieldedOutput > 0.0
               'migration'
             else
@@ -61,9 +67,5 @@ class Transaction < ApplicationRecord
     else
       return nil
     end
-
-
-    # Also need to add migration for classification column in the db
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_192123) do
+ActiveRecord::Schema.define(version: 2020_12_04_015031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_192123) do
     t.boolean "overwintered"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
   end
 
 end
