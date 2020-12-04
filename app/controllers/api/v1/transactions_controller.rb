76fab40be826085d4742f9834cf641b9a76ea2d5
Elliot Blanchard <git@elliotblanchard.com>
2020-12-04 10:55:15 -0500

    Added activeRecord request to total each category type for that time range

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -5,6 +5,13 @@ class Api::V1::TransactionsController < ApplicationController
 
     # For now simply returning all transactions from previous day (GMT)
     # Needs to be hourly totals for each transaction type
+
+    # This gets the totals for each category for a given time range:
+    # Transaction.group(:category).where(timestamp: 1606953601..1607039999).count
+    # Returns
+    # => {"sapling_shielded"=>106, "transparent"=>3072, nil=>14, "sapling_deshielding"=>253, 
+    # "transparent_coinbase"=>1137, "sprout_deshielding"=>4, "sapling_shielding"=>214}
+    
     @transactions = Transaction.where(timestamp: epoch_range[:start]..epoch_range[:end])
     
     render json: @transactions
@@ -23,6 +30,9 @@ class Api::V1::TransactionsController < ApplicationController
     # date.to_time.in_time_zone('London').end_of_day.to_i (convert to Unix Epoch time)
 
     # Make sure to specify time zone (GMT)
+
+    # Example values:
+    # Dec 3 range: 1606953601 - 1607039999
 
     epoch_range = {}
     epoch_range[:start] = start_time.to_time.in_time_zone('London').beginning_of_day.to_i
