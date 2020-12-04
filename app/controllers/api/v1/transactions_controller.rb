f8b7af3c071a646ae0b59764a52ee73f77468202
Elliot Blanchard <git@elliotblanchard.com>
2020-12-04 17:13:44 -0500

    transaction index route now correctly returns transaction category totals hourly for  previous day

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::TransactionsController < ApplicationController
   def index
     # Replace this Date with a Time object for yesterday:
     # 1.day.ago
-    epoch_range = datetime_to_epoch_range(Date.yesterday)
+    # epoch_range = datetime_to_epoch_range(Date.yesterday)
+    # epoch_range = datetime_to_epoch_range(1.day.ago)
 
     # For now simply returning all transactions from previous day (GMT)
     # Needs to be hourly totals for each transaction type
@@ -14,16 +15,49 @@ class Api::V1::TransactionsController < ApplicationController
     # => {"sapling_shielded"=>106, "transparent"=>3072, nil=>14, "sapling_deshielding"=>253, 
     # "transparent_coinbase"=>1137, "sprout_deshielding"=>4, "sapling_shielding"=>214}
     
-    @transactions = Transaction.where(timestamp: epoch_range[:start]..epoch_range[:end])
+    # @transactions = Transaction.where(timestamp: epoch_range[:start]..epoch_range[:end])
     
+    @transactions = get_transactions('hourly', 1.day.ago)
     render json: @transactions
   end
 
   private
 
-  def datetime_to_epoch_range(start_time, end_time = nil)
-    # Converts datetime(s) to a range of epoch times
-    # Will return epoch time from beginning to end of specified day
+  def get_transactions(time_unit, time)
+    utc_offset = '+00:00'
+    response = {}
+    header = {}
+    body = {}
+    transactions = []
+
+    header[:time_unit] = time_unit
+    response[:header] = header
+
+    # Needs to be a check in here for the 'weekly', 'monthly', 'quarterly' that it dosen't 
+    # try to go past the current day - 1
+
+    if time_unit == 'hourly'
+      for i in 0..23
+        hour = Time.new(time.year, time.month, time.day, i, 0, 0, utc_offset)
+        epoch_range = time_to_epoch_range(time_unit, hour)
+        time_interval = {}
+        time_interval[:time] = hour.to_i
+        time_interval[:total] = Transaction.where(timestamp: epoch_range[:start]..epoch_range[:end]).count
+        # Next line is only for QA remove for performance
+        time_interval[:example_hash] = Transaction.where(timestamp: epoch_range[:start]..epoch_range[:end]).last.zhash
+        time_interval[:transactions] = Transaction.group(:category).where(timestamp: epoch_range[:start]..epoch_range[:end]).count
+        transactions.push(time_interval)
+      end
+    end
+
+    body[:transactions] = transactions
+    response[:body] = body
+
+    response
+  end
+
+  def time_to_epoch_range(time_unit, time)
+    # Converts time object to a range of epoch times
 
     # Sequence to get beginning and end of a date:
     # date = Date.today
@@ -43,12 +77,10 @@ class Api::V1::TransactionsController < ApplicationController
     # Dec 3 range: 1606953601 - 1607039999
 
     epoch_range = {}
-    epoch_range[:start] = start_time.to_time.in_time_zone('London').beginning_of_day.to_i
 
-    if end_time
-      epoch_range[:end] = end_time.to_time.in_time_zone('London').end_of_day.to_i
-    else
-      epoch_range[:end] = start_time.to_time.in_time_zone('London').end_of_day.to_i
+    if time_unit == 'hourly'
+      epoch_range[:start] = time.beginning_of_hour.to_i
+      epoch_range[:end] = time.end_of_hour.to_i
     end
 
     epoch_range
