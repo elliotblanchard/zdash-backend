93144f0183cec7a30af60219df9440ccd8bc2518
Elliot Blanchard <git@elliotblanchard.com>
2021-01-09 14:17:59 -0500

    Small test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     #origins '*'
-     origins 'https://damp-citadel-47471.herokuapp.com/'
+     origins '*'
+     #origins 'https://damp-citadel-47471.herokuapp.com/'
 
      resource '*',
        headers: :any,
