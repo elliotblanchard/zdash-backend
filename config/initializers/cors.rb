edf1248fa89e2aaec345fb8692d3b6b646a5e2ad
Elliot Blanchard <git@elliotblanchard.com>
2021-01-09 14:31:03 -0500

    CORS update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
      #origins '*'
-     origins 'zdash.info'
+     origins 'https://www.zdash.info'
 
      resource '*',
        headers: :any,
