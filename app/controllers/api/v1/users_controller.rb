7551f24dd7fe731f8515c859d318c61704f1bfab
Elliot Blanchard <git@elliotblanchard.com>
2021-02-06 17:48:15 -0500

    Added user name update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,22 @@
 class Api::V1::UsersController < ApplicationController
 
-  #def index
-  #  @users = User.all
-  #  render json: @users, status: :accepted
-  #end
-
   def create
-    @user = User.create(user_params)
+    if (@user = User.find_by(address: user_params[:address]))
+      @user.update(name: user_params[:name])
+    else
+      @user = User.create(user_params)
+    end
     render json: @user, status: :created
   end
 
   def show
-    if (@user = User.find_by(address: user_params[:id]))
+    if (@user = User.find_by(address: params[:id]))
       render json: @user, status: :accepted
     else
-      render json: { message: 'No name for this address' }, status: :not_found
+      @user = {'name': '', 'address': ''}
+      render json: @user, status: :accepted
     end
-  end  
+  end
 
   private
 
